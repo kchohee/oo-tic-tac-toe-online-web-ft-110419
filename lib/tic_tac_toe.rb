fdf63a827287d2de714c6a70e4666adcb1878409
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-29 01:42:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,8 +6,9 @@ class TicTacToe
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
   def display_board
     puts row_1 = " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts divider = "-----------"
     puts row_2 = " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts divider
     puts row_3 = " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-     div = "-----------"
    end
 end
