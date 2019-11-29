776f475f145d8026840190ab574d661a72b5b9cc
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-29 21:46:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,4 +66,15 @@ class TicTacToe
     end
     return false
   end
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move? (index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
 end
