c981b6c6613ee0bde5939c8da35e1542cb2c4a10
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-29 21:41:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,4 +31,39 @@ class TicTacToe
       false
     end
   end
+  def turn_count
+    @counter = 0
+      @board.each do |token|
+        if token != " "
+          @counter += 1
+        end
+      end
+    return @counter
+  end
+
+  def current_player
+    turn_count
+    if @counter % 2 == 0
+      return "X"
+    else
+      return "O"
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      position_1 = @board[win_index_1]
+      position_2 = @board[win_index_2]
+      position_3 = @board[win_index_3]
+
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X")  || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return win_combination
+      end
+    end
+    return false
+  end
 end
