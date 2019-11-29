4014a353db903175614831de81fd708ffd57c06e
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-29 01:40:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,5 +4,10 @@ class TicTacToe
     @board = Array.new(9," ")
   end
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
-
+  def display_board
+    puts row_1 = " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts row_2 = " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts row_3 = " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+     div = "-----------"
+   end
 end
